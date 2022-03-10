33f1d8828dfddfb0c4c43070496fedb407d5bfc6
Thanhtb01 <thanhquachtb@gmail.com>
2022-03-10 21:02:03 +0700

    dongngu

---
+++ untitled/src/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Nhap i: ");
+        int i;
+        Scanner sc = new Scanner(System.in);
+        i = sc.nextInt();
+        System.out.println("i="+i);
+    }
+}
